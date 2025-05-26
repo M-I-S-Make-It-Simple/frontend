859bdcf919e29ef147f1a89cb07b061db57f246c
Inna Skakunova <innesas022@gmail.com>
2025-05-26 08:33:27 +0300

    adjust header styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,109 +7,134 @@ import styles from "../../styles/HeaderFooter.module.css";
 
 const Header = () => {
   return (
-  <header className={styles.header}>
-  <nav className={styles.navContainer}>
-    <div className={styles.logo}>
-      <Image src={headerLogo} alt="Logo" width={52} height={52} />
-    </div>
-
-    <div className={styles.navLinks}>
-      <div className={`${styles.navItem} ${styles.dropdown}`}>
-        <a className={styles.navLink} href="index.html">
-          <span>Про</span><span>ліцей</span>
-        </a>
-        <div className={styles.dropdownContent}>
-          <a href="visitingcard.html">Наша візитка</a>
-          <a href="history.html">Історія закладу</a>
-          <a href="innovative.html">Інноваційна діяльність</a>
+    <header className={styles.header}>
+      <nav className={styles.navContainer}>
+        <div className={styles.logo}>
+          <Image src={headerLogo} alt="Logo" width={52} height={52} />
         </div>
-      </div>
 
-      <a className={styles.navLink} href="news.html">
-        <span>Новини</span>
-      </a>
+        <div className={styles.navLinks}>
+          <div className={`${styles.navItem} ${styles.dropdown}`}>
+            <Link className={styles.navLink} href="/">
+              <span>Про</span>
+              <span>ліцей</span>
+            </Link>
+            <div className={styles.dropdownContent}>
+              <Link href="/visitingcard">Наша візитка</Link>
+              <Link href="/history">Історія закладу</Link>
+              <Link href="/innovative">Інноваційна діяльність</Link>
+            </div>
+          </div>
 
-      <div className={styles.navItem}>
-        <a className={styles.navLink} href="teaching-staff.html">
-          <span>Педагогічний</span><span>колектив</span>
-        </a>
-      </div>
+          <Link className={styles.navLink} href="/news">
+            <span>Новини</span>
+          </Link>
 
-      <div className={`${styles.navItem} ${styles.dropdown}`}>
-        <a className={styles.navLink} href="#">
-          <span>Прозорість</span><span>управління</span>
-        </a>
-        <div className={styles.dropdownContent}>
-          <a href="regulatory-documents.html">Нормативні документи</a>
-          <a href="financial-reports.html">Фінансова звітність</a>
-          <a href="public-information.html">Публічна інформація</a>
+          <div className={styles.navItem}>
+            <Link className={styles.navLink} href="/teaching-staff">
+              <span>Педагогічний</span>
+              <span>колектив</span>
+            </Link>
+          </div>
+
+          <div className={`${styles.navItem} ${styles.dropdown}`}>
+            <Link className={styles.navLink} href="#">
+              <span>Прозорість</span>
+              <span>управління</span>
+            </Link>
+            <div className={styles.dropdownContent}>
+              <Link href="/regulatory-documents">Нормативні документи</Link>
+              <Link href="/financial-reports">Фінансова звітність</Link>
+              <Link href="/public-information">Публічна інформація</Link>
+            </div>
+          </div>
+
+          <div className={`${styles.navItem} ${styles.dropdown}`}>
+            <Link className={styles.navLink} href="#">
+              <span>Освітній</span>
+              <span>процес</span>
+            </Link>
+            <div className={styles.dropdownContent}>
+              <Link href="/intellect">Інтелект та обдарованість</Link>
+              <Link href="/student-self-government">
+                Учнівське самоврядування
+              </Link>
+              <Link href="/project-research">
+                Проєктно-дослідницька діяльність
+              </Link>
+              <Link href="/patriotic-education">
+                Національно-патріотичне виховання
+              </Link>
+              <Link href="/evaluation-criteria">Критерії оцінювання</Link>
+              <Link href="/career-guidance">Профорієнтаційна сторінка</Link>
+              <Link href="/moral-education">Морально-етичне виховання</Link>
+              <Link href="/clubs-studios">Клуби та студії</Link>
+              <Link href="/sportlife">СпортLife</Link>
+              <Link href="/psychological-support">
+                Соціально-психологічна підтримка
+              </Link>
+              <Link href="/anti-bullying">Протидія булінгу</Link>
+            </div>
+          </div>
+
+          <div className={`${styles.navItem} ${styles.dropdown}`}>
+            <Link className={styles.navLink} href="#">
+              <span>Методична</span>
+              <span>робота</span>
+            </Link>
+            <div className={styles.dropdownContent}>
+              <Link href="/teacher-help">На допомогу вчителю</Link>
+              <Link href="/qualification-improvement">
+                Підвищення кваліфікації
+              </Link>
+              <Link href="/teacher-certification">
+                Атестація педпрацівників
+              </Link>
+              <Link href="/methodical-events">Основні методичні заходи</Link>
+              <Link href="pedagogical-olympus.html">Педагогічний Олімп</Link>
+              <Link href="methodical-lifehacks.html">Методичні лайфхаки</Link>
+            </div>
+          </div>
+
+          <div className={`${styles.navItem} ${styles.dropdown}`}>
+            <Link className={styles.navLink} href="#">
+              <span>Інформаційна</span>
+              <span>сторінка</span>
+            </Link>
+            <div className={styles.dropdownContent}>
+              <Link href="#">Батькам</Link>
+              <Link href="#">Учням</Link>
+            </div>
+          </div>
+
+          <div className={`${styles.navItem} ${styles.dropdown}`}>
+            <Link className={styles.navLink} href="#">
+              <span>Інше</span>
+            </Link>
+            <div className={styles.dropdownContent}>
+              <Link href="#">Літературний сайт Володимира Малика</Link>
+              <Link href="#">Лубенська міська рада</Link>
+              <Link href="#">Міністерство освіти і науки України</Link>
+              <Link href="#">Український центр оцінювання якості освіти</Link>
+              <Link href="#">
+                Харківський регіональний центр оцінювання якості освіти
+              </Link>
+            </div>
+          </div>
         </div>
-      </div>
 
-      <div className={`${styles.navItem} ${styles.dropdown}`}>
-        <a className={styles.navLink} href="#">
-          <span>Освітній</span><span>процес</span>
-        </a>
-        <div className={styles.dropdownContent}>
-          <a href="intellect.html">Інтелект та обдарованість</a>
-          <a href="student-self-government.html">Учнівське самоврядування</a>
-          <a href="project-research.html">Проєктно-дослідницька діяльність</a>
-          <a href="patriotic-education.html">Національно-патріотичне виховання</a>
-          <a href="evaluation-criteria.html">Критерії оцінювання</a>
-          <a href="career-guidance.html">Профорієнтаційна сторінка</a>
-          <a href="moral-education.html">Морально-етичне виховання</a>
-          <a href="clubs-studios.html">Клуби та студії</a>
-          <a href="sportlife.html">СпортLife</a>
-          <a href="psychological-support.html">Соціально-психологічна підтримка</a>
-          <a href="anti-bullying.html">Протидія булінгу</a>
+        <div className={styles.languageSwitcher}>
+          <Image
+            src={languageChange}
+            alt="languageChange"
+            width={32}
+            height={38}
+          />
         </div>
-      </div>
+      </nav>
 
-      <div className={`${styles.navItem} ${styles.dropdown}`}>
-        <a className={styles.navLink} href="#">
-          <span>Методична</span><span>робота</span>
-        </a>
-        <div className={styles.dropdownContent}>
-          <a href="teacher-help.html">На допомогу вчителю</a>
-          <a href="qualification-improvement.html">Підвищення кваліфікації</a>
-          <a href="teacher-certification.html">Атестація педпрацівників</a>
-          <a href="methodical-events.html">Основні методичні заходи</a>
-          <a href="pedagogical-olympus.html">Педагогічний Олімп</a>
-          <a href="methodical-lifehacks.html">Методичні лайфхаки</a>
-        </div>
-      </div>
-
-      <div className={`${styles.navItem} ${styles.dropdown}`}>
-        <a className={styles.navLink} href="#">
-          <span>Інформаційна</span><span>сторінка</span>
-        </a>
-        <div className={styles.dropdownContent}>
-          <a href="#">Батькам</a>
-          <a href="#">Учням</a>
-        </div>
-      </div>
-
-      <div className={`${styles.navItem} ${styles.dropdown}`}>
-        <a className={styles.navLink} href="#">
-          <span>Інше</span>
-        </a>
-        <div className={styles.dropdownContent}>
-          <a href="#">Літературний сайт Володимира Малика</a>
-          <a href="#">Лубенська міська рада</a>
-          <a href="#">Міністерство освіти і науки України</a>
-          <a href="#">Український центр оцінювання якості освіти</a>
-          <a href="#">Харківський регіональний центр оцінювання якості освіти</a>
-        </div>
-      </div>
-    </div>
-
-    <div className={styles.languageSwitcher}>
-      <Image src={languageChange} alt="languageChange" width={32} height={38} />
-    </div>
-  </nav>
-
-  <hr />
-</header>
+      <hr />
+    </header>
   );
 };
 
